2a96ed3e0ca28228b8efdb8c392c4964a95c782c
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2023-08-05 15:53:48 +0530

    Ternary Operators and CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <UserProfileComponent
-        name="obito"
-        handle="@gwa99a9"
-        email="jamesheller.jeewake@gmail.com"
-      />
-      <Job salary={80000} position="Software Engineer" company="Virtusa" />
-      <UserProfileComponent
-        name="Jeewaka"
-        handle="@gwa99a9"
-        email="jamesheller.jeewake@gmail.com"
-      />
-    </div>
-  );
+  const age = 159;
+  if (age >= 18) {
+    return (
+      <div className={styles.App}>
+        <UserProfileComponent
+          name="obito"
+          handle="@gwa99a9"
+          email="jamesheller.jeewake@gmail.com"
+        />
+        <Job salary={80000} position="Software Engineer" company="Virtusa" />
+        <UserProfileComponent
+          name="Jeewaka"
+          handle="@gwa99a9"
+          email="jamesheller.jeewake@gmail.com"
+        />
+         <Job salary={120000} position="Engineer" company="Virtusa" />
+      </div>
+    );
+  } else {
+    return "Under age";
+  }
 }
 
 const UserProfileComponent = (props) => {
   return (
-    <div className="UserProfileComponent">
+    <div className={styles.UserProfileComponent}>
       <h1>{props.name}</h1>
       <h1>{props.handle}</h1>
       <h1>{props.email}</h1>
@@ -32,6 +38,7 @@ const Job = (props) => {
   return (
     <div>
       <h1>{props.salary}</h1>
+      <h2 style={{color: props.salary>=100000 ? "green" : "red"}}>{props.salary>=100000 ? "Good Salary" : "Bad Salary"}</h2>
       <h1>{props.position}</h1>
       <h1>{props.company}</h1>
     </div>
